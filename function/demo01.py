c89fdbb95e35acd32039309c454fb64529686a42
kevin <948876386@qq.com>
2020-01-29 23:33:00 +0800

    init

---
+++ function/demo01.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python2
+#-*- coding:UTF-8 -*-
+
+def myFirstFunction():
+    print('1')
+    print('2')
+    print('3')
+    x = 4
+    y = 5 
+    z= x+y
+    print(z)
+
+for i in range(3):
+    myFirstFunction()
