6f4377cb8508fcb9d17cce3bfb24e13b781b2353
kevin <948876386@qq.com>
2020-01-29 23:21:28 +0800

    init

---
+++ demo00.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python2
+#-*- coding:UTF-8 -*-
+
+def myFirstFunction():
+    print("这是我创建的第一个函数！")
+    print("我表示很激动......")
+    print("在这里，我要感谢TVB，感谢CCTV！")
+
+if __name__ == "__main__":
+     myFirstFunction()
+
