0ae009812951588bee201b7be303f68e2ade598c
kevin <948876386@qq.com>
2020-02-04 00:35:25 +0800

    init

--- list_append.py
+++ list_append.py
@@ -2,7 +2,14 @@
 #-*- coding:UTF-8 -*-
 
 stu = ['id','name','sex','age','course']
+
+#append()方法支持追加1个参数
 stu.append(['english','math'])
 
 print(stu)
 print(type(stu))
+
+#extend()方法支持以列表方式追加参数
+stu.extend(['5','8',['sky','kevin'],'women'])
+
+print(stu)
